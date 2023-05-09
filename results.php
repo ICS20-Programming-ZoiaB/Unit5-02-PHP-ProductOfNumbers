6845b38494341d0f8a3a8975316582162308e292
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-09 19:25:06 +0000

    Used abs to allow the loop to use absolute values of numbers.

--- results.php
+++ results.php
@@ -1,17 +1,22 @@
 <?php
 
-  //initialize sum variable
-  $sum = 0;
+  //initialize product variable
+  $product = 0;
 
   //getting the user's two numbers to find the product
   $firstNum = floatval($_POST['first-number']);
   $secondNum = floatval($_POST['second-number']);
 
   //using a for loop to do repeated addition to get the product
-  for ($counter = 1; $counter <= $secondNum; $counter++) {
-    $sum = $sum + $firstNum;
+  for ($counter = 1; $counter <= abs($secondNum); $counter++) {
+    $product = $product + abs($firstNum);
   }
 
-  //displaying the sum/product to user
-  echo "The product of your two selected numbers is " . $sum . ".";
+  //If one of the two entered integers was negative, multiply by -1 to add back negative
+  if (($firstNum < 0 && $secondNum > 0) || ($firstNum > 0 && $secondNum < 0)) {
+    $product = -$product;
+  }
+
+  //displaying the product/product to user
+  echo "The product of your two selected numbers is " . $product . ".";
 ?>
