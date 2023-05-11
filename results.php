ee8a21dd923330ab4440af5b45efb34df73b2f0c
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-11 02:22:21 +0000

    Added error message if user enters nothing

--- results.php
+++ results.php
@@ -4,13 +4,20 @@
   $product = 0;
 
   //getting the user's two numbers to find the product
-  $firstNum = floatval($_POST['first-number']);
-  $secondNum = floatval($_POST['second-number']);
+  $firstNum = $_POST['first-number'];
+  $secondNum = $_POST['second-number'];
 
-  //using a for loop to do repeated addition to get the product
-  for ($counter = 1; $counter <= abs($secondNum); $counter++) {
-    $product = $product + abs($firstNum);
+  //If statement to give an error message if nothing nothing is entered or if numbers are invalid
+  if ((!is_numeric($firstNum) || !is_numeric($secondNum)) || ($firstNum == "" || $secondNum == "")) {
+    echo "Please enter valid numbers.";
   }
+
+  //Else
+  else {
+    //using a for loop to do repeated addition to get the product
+    for ($counter = 1; $counter <= abs($secondNum); $counter++) {
+      $product = $product + abs($firstNum);
+    }
 
   //Add back negative sign if needed
   if (($firstNum < 0 && $secondNum > 0) || ($firstNum > 0 && $secondNum < 0)) {
@@ -19,4 +26,5 @@
 
   //displaying the product/product to user
   echo "The product of your two selected numbers is " . $product . ".";
+}
 ?>
